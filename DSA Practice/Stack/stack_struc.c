4278a3ddd5ea11bc8c7e2b35d5b4570308651d75
Abhiram Laha <abhiramlaha2020@gmail.com>
2023-01-03 00:07:14 +0530

    stack

--- DSA Practice/Stack/stack_struc.c
+++ DSA Practice/Stack/stack_struc.c
@@ -19,6 +19,57 @@ void PUSH(int size,int data){
     }
     else{
         s.top++;
-        
+        s.arr[s.top]=data;
+        //printf("\nData Pushed!!");
     }
+}
+
+// ? POP
+
+void POP(){
+    if(s.top==-1){
+        printf("Stack Empty!!");
+    }
+    else{
+        int v=s.arr[s.top];
+        s.top--;
+
+        printf("\n%d is Poped!!",v);
+    }
+}
+
+
+// ?  DISPLAY
+
+void DISPLAY(int size){
+    if(s.top==-1){
+        printf("Stack Empty!!");
+    }
+    else{
+        printf("\nStack : ");
+        for(int i=s.top; i>=0; i--){
+            printf("%d ",s.arr[i]);
+        }
+    }
+}
+
+
+int main(){
+
+    s.top=-1;
+    int s=5;
+
+    PUSH(s,5);
+    PUSH(s,4);
+    PUSH(s,3);
+    PUSH(s,2);
+    PUSH(s,1);
+
+    DISPLAY(s);
+
+    POP();
+
+    DISPLAY(s);
+
+    return 0;
 }
