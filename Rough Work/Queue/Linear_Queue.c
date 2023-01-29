2ad51e4f7fbf7f56b61dcbd8b83fdb99b5fa5ad0
Abhiram Laha <abhiramlaha2020@gmail.com>
2023-01-30 01:46:16 +0530

    update

--- Rough Work/Queue/Linear_Queue.c
+++ Rough Work/Queue/Linear_Queue.c
@@ -9,84 +9,84 @@ typedef struct queue{
 queue q;
 int size=20;
 
-int isFull(){
-    return (q.front==(q.rear+1)%size);
-}
-
 int isEmpty(){
-    return (q.front==-1 && q.rear==-1);
+    if(q.front==-1 && q.rear==-1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
 
-//? ENQUEUE
+int isFull(){
+    return (q.rear==size-1);
+}
+
+//! ENQUEUE
 
 void Enqueue(int data){
     if(isFull()){
-        printf("Queue Overflow.");
+        printf("\nQueue Overflow.");
     }
     else if(isEmpty()){
-        q.front=0;
-        q.rear=0;
+        q.front++;
+        q.rear++;
     }
     else{
-        q.rear=(q.rear+1)%size;
+        q.rear++;
     }
 
     q.arr[q.rear]=data;
 }
 
 
-//? DEQUEUE
+//! DEQUEUE
 
 int Dequeue(){
     int v=q.arr[q.front];
-
     if(isEmpty()){
-        printf("\nUnderFlow");
+        printf("\nEmpty Queue");
     }
     else if(q.front==q.rear){
         q.front=-1;
         q.rear=-1;
     }
+
     else{
-        q.front=(q.front+1)%size;
+        q.front++;
     }
 
     return v;
 }
 
-//? DISPLAY
+
+//! DISPLAY
 
 void display(){
-    if(isEmpty()){
-        printf("Empty");
-    }
-    else{
-        printf("\nQueue : ");
-
-        int x=q.front;
-        for(int i=1; i<=size; i++){
-            printf("%d ",q.arr[x]);
-
-            x=(x+1)%size;
-
-        }
+    printf("\nQueue : ");
+    for(int i=q.front; i<=q.rear; i++){
+        printf("%d ",q.arr[i]);
     }
 }
 
+
 int main(){
-    q.front=-1;
-    q.rear=-1;
+
     size=5;
+    q.rear=-1;
+    q.front=-1;
 
     Enqueue(1);
     Enqueue(2);
     Enqueue(3);
+    //Dequeue();
     Enqueue(4);
     Enqueue(5);
 
-
+    printf("%d",Dequeue());
 
     display();
+
     return 0;
 }
